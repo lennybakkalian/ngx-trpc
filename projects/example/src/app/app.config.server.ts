e6b687b041ecac8c53c268d5d2b838e902e01782
lenny <lenny.bakkalian@gmail.com>
2024-11-23 13:18:39 +0100

    feat: update docs

--- projects/example/src/app/app.config.server.ts
+++ projects/example/src/app/app.config.server.ts
@@ -1,9 +1,13 @@
 import {ApplicationConfig, mergeApplicationConfig} from '@angular/core';
 import {provideServerRendering} from '@angular/platform-server';
 import {appConfig} from './app.config';
+import {provideServerRoutesConfig, RenderMode} from '@angular/ssr';
 
 const serverConfig: ApplicationConfig = {
-  providers: [provideServerRendering()]
+  providers: [
+    provideServerRendering(),
+    provideServerRoutesConfig([{path: '**', renderMode: RenderMode.Server}])
+  ]
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
