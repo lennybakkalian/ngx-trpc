bcc78cf3e9016d1af686d3ed6da6253f95e77e59
lenny <lenny.bakkalian@gmail.com>
2024-12-23 16:48:31 +0100

    feat: intercept fetch errors and print them in debug mode

--- projects/ngx-trpc/src/lib/utils/fetch.middleware.ts
+++ projects/ngx-trpc/src/lib/utils/fetch.middleware.ts
@@ -18,56 +18,67 @@ export class FetchMiddleware {
     // This should not be a problem, since we use batch calls.
     return wrapInMutex(
       async () => {
-        if (typeof input != 'string') {
-          throw new Error('[ngx-trpc] Only string urls are supported right now.');
-        }
-
-        if (this._request) {
-          const headers: HeadersInit = {};
-          this._request.headers.forEach((value: string, key: string) => {
-            headers[key] = value;
-          });
-
-          if (this._setCookiesCache) {
-            const cookies = cookie.parse(headers['cookie'] || '');
-            const newCookies = cookie.parse(this._setCookiesCache);
-
-            for (let key in newCookies) {
-              cookies[key] = newCookies[key];
-            }
-
-            headers['cookie'] = Object.entries(cookies)
-              .filter(([_, value]) => value !== undefined)
-              .map(([key, value]) => cookie.serialize(key, value!))
-              .join('; ');
+        try {
+          if (typeof input != 'string') {
+            throw new Error('[ngx-trpc] Only string urls are supported right now.');
           }
 
-          init = {...init, headers};
-        }
+          if (this._request) {
+            const headers: HeadersInit = {};
+            this._request.headers.forEach((value: string, key: string) => {
+              headers[key] = value;
+            });
 
-        if (this._config.debug) {
-          console.log('[ngx-trpc] fetch', input);
-        }
+            if (this._setCookiesCache) {
+              const cookies = cookie.parse(headers['cookie'] || '');
+              const newCookies = cookie.parse(this._setCookiesCache);
 
-        const r = await fetch(input, init).catch((e) => {
+              for (let key in newCookies) {
+                cookies[key] = newCookies[key];
+              }
+
+              headers['cookie'] = Object.entries(cookies)
+                .filter(([_, value]) => value !== undefined)
+                .map(([key, value]) => cookie.serialize(key, value!))
+                .join('; ');
+            }
+
+            init = {...init, headers};
+          }
+
           if (this._config.debug) {
-            console.error('[ngx-trpc] fetch error', e);
+            console.log('[ngx-trpc] fetch', input);
           }
-          return e;
-        });
-        if (this._response && this._response.headers && this._response.headers instanceof Headers) {
-          for (let [key, value] of r.headers) {
-            if (this._config.ssr?.forwardHeaders?.includes(key) || key === 'set-cookie') {
-              this._response.headers.set(key, value);
-            }
 
-            // set the cookie in the angular request object so we can use it in sequential requests when not using batchLink
-            if (key == 'set-cookie') {
-              this._setCookiesCache = value;
+          const r = await fetch(input, init).catch((e) => {
+            if (this._config.debug) {
+              console.error('[ngx-trpc] fetch error', e);
+            }
+            return e;
+          });
+          if (
+            this._response &&
+            this._response.headers &&
+            this._response.headers instanceof Headers
+          ) {
+            for (let [key, value] of r.headers) {
+              if (this._config.ssr?.forwardHeaders?.includes(key) || key === 'set-cookie') {
+                this._response.headers.set(key, value);
+              }
+
+              // set the cookie in the angular request object so we can use it in sequential requests when not using batchLink
+              if (key == 'set-cookie') {
+                this._setCookiesCache = value;
+              }
             }
           }
+          return r;
+        } catch (e) {
+          if (this._config.debug) {
+            console.error('[ngx-trpc] fetch middleware error', e);
+          }
+          throw e;
         }
-        return r;
       },
       this._mutex,
       this._config.ssr?.disableSequentialRequests
